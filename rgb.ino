53c6b73cdd21d5d9b6dfacd3f40f5c5c3816c373
smriti halder <65438599+smritihalder@users.noreply.github.com>
2020-07-14 18:56:13 +0530

    Add files via upload

---
+++ rgb.ino
@@ -0,0 +1,46 @@
+int redpin=10;
+int greenpin=11;
+int bluepin=9;
+
+
+void setup() {
+  
+
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+ 
+colour(225,0,0);//red
+delay(1000);
+colour(0,225,0);//green
+delay(1000);
+colour(0,0,225);//blue
+delay(1000);
+colour(225,225,225);//white
+delay(1000);
+colour(170,0,225);
+delay(1000);
+colour(150,0,225);
+delay(1000);
+colour(200, 255,255);
+delay(1000);
+colour(200,0,0);
+delay(1000);
+colour(255,0,255);//magenta
+delay(1000);
+colour(255,255,0);//yellow
+delay(1000);
+colour(0,255,255);//cyan
+delay(1000);
+colour(255,255,125);//raspberry
+delay(1000);
+}
+void colour(int redvalue,int greenvalue,int bluevalue)
+{
+  analogWrite(redpin,redvalue);
+  analogWrite(greenpin,greenvalue);
+  analogWrite(bluepin,bluevalue);
+}
